3c10f01bb6f79c636bdbd8ce0b3aa5bcfde7e2a4
ajitj <ajitk123@gmail.com>
2025-07-30 10:42:06 +0530

    datatypes updates

---
+++ basic/Values.nim
@@ -0,0 +1,15 @@
+echo "Hello!"
+echo "nim" & "ble"
+
+echo 15 + 6
+echo 33.5 / 2
+
+echo true and false
+echo true or false
+echo not true
+
+echo "15 + 3 = ..."
+echo $(15 + 3)
+
+echo "not true is " & $(not true)
+echo "not false is " & $(not false)
